b3ccd8e9375fe9dac0113a40a6abf01b346714d9
MaxinFN <maxindeadpool@gemail.com>
2019-07-25 21:12:49 +0800

    push组件请求数据

--- src/components/consult/push/pushItem.jsx
+++ src/components/consult/push/pushItem.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
+// moment
+import moment from 'moment';
+
 import './pushitem.scss';
 
 
@@ -13,12 +17,42 @@ class PushItem extends Component {
         }
 
         this.getdata = this.getdata.bind(this);
+        this.scroll = this.scroll.bind(this);
+
     }
 
     async getdata() {
-        let news = (await axios.get("http://localhost:3100/api/stock/stock-rank?plate_id=3000005&_=" + this.state.time)).data
+        let news = (await axios.get("http://localhost:3100/my/client/market-list?news_id=" + this.state.page)).data.data.list
 
         console.log(news)
+        // 延迟加载
+        await new Promise((resolve) => {
+            setTimeout(() => {
+                resolve()
+            }, 500);
+        })
+
+        this.setState({
+            news: [...this.state.news, ...news],
+            page: ++this.state.page,
+        })
+
+        console.log("this.state.page", this.state.page)
+
+    }
+
+    scroll() {
+        let scrollTop = document.documentElement.scrollTop || document.getElementsByClassName("push-item")[0].scrollTop;
+        let windowHeight = document.getElementsByClassName("push-item")[0].clientHeight;
+        let scrollHeight = document.getElementsByClassName("push-item")[0].scrollHeight;
+
+        let ih = scrollHeight - scrollTop - windowHeight
+
+        console.log(scrollTop, windowHeight, scrollHeight, ih)
+
+        if (ih <= 1) {
+            this.getdata()
+        }
     }
 
     componentWillMount() {
@@ -26,9 +60,10 @@ class PushItem extends Component {
     }
 
     render() {
+        // console.log("this.state.news", this.state.news)
         return (
-            <div className="push-item">
-                <div className="item">
+            <div className="push-item" onScroll={this.scroll}>
+                {/* <div className="item">
                     <div className="item-left">
                         <h3 className="push-title">push-title</h3>
                         <p className="time">time</p>
@@ -37,7 +72,24 @@ class PushItem extends Component {
                         item-right
                         <img className="push-img" src="" alt="" />
                     </div>
-                </div>
+                </div> */}
+                {
+                    this.state.news.map(item => {
+                        return (
+                            <div className="item" key={item.news_id}>
+                                <div className="item-left">
+                                    <h3 className="push-title">{item.title}</h3>
+                                    <p className="time">{
+                                        moment(item.time).format('HH:mm')}</p>
+                                </div>
+                                <div className="item-right">
+
+                                    <img className="push-img" src={item.pic} alt="" />
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
